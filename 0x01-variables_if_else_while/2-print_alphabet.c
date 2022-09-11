646f8fda08309d513a91263125f4de92f9d3b795
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-12 00:12:33 +0100

    new

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
