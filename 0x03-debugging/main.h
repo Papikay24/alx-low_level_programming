c6dadad4bb2630e8fdd843ccdeea0b351967ebb6
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-15 22:40:32 +0100

    new

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days();
 
 int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
