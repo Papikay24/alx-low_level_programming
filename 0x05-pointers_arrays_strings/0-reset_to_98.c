2b4e8bba1d446efd4cb7faf742dd0b7c6e79a198
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-19 18:38:03 +0100

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,15 +10,15 @@
 
 void reset_to_98(int *n)
 {
-	int n;
+	int m = 0;
 
-	n = 0;
+	int *n;
 
-	int *n = &n;
+	n = &m
 
 	printf("n=%d\n", n);
 
-	*n = 98;
+	n = 98;
 	printf("n=%d\n", n);
 
 	return (0);
