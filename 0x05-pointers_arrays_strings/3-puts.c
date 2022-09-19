cbc4f5996af4f3a62f22a170bd783bacb9a75e1a
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-19 21:24:57 +0100

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: character value
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str) > stdout;
+}
