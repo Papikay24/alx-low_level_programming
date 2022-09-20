0f104ee73a63a6de88240a84e63fc67390a0793c
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-20 09:40:27 +0100

    fixes

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,8 +5,23 @@
  * @s: input
  * Return: Always 0 (Success)
  */
+
 int _atoi(char *s)
 {
+	int n;
 
+	s = n;
+	if (; s = ' ')
+	{
+		break;
+
+		else
+		       continue;
+
+		if (s >= 0 || s <= 0)
+		{
+			printf("%d\n", s);
+		}
+	}
 	return (0);
 }
