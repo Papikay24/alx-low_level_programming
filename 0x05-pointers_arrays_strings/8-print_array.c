ac26332c2c47c64283dc5c85cea87b443a615df3
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-20 09:57:25 +0100

    new

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < (n); i++)
 	{
-		printf("%d, ", *a[i]);
+		printf("%d, ", a[i]);
 
 		if (i == (n))
 		{
-			printf("%d", *a[n]);
+			printf("%d", a[n]);
 		}
 	}
 	printf("\n");
