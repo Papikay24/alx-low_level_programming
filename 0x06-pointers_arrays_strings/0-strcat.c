572014f911586a2cffaabb7c682ff0eb5d9e77b3
abdulkarim <abdulkarimkrim6789@gmail.com>
2022-09-22 12:17:13 +0100

    catenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,11 +3,12 @@
 
 /**
  * *_strcat - a function that concatenates two strings
- *
+ * @dest: character input
+ * @src: character input
  * Return: a pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
-	return(*dest);
+	return (*dest);
 }
